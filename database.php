027a319c2e5350662671e07c0398f4eb7206c288
Keiocho <117529892+Keiocho@users.noreply.github.com>
2025-05-02 12:43:08 -0400

    Add files via upload

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+// Enable error reporting (optional but helpful)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "campuscart";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+}
+?>
